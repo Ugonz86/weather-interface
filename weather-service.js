085c84896e49c29d3370e6c0349e19ca58c32cba
Ugonz86 <ugonzalez86@gmail.com>
2019-07-02 20:13:19 -0700

    This will commit the initial work

---
+++ weather-service.js
@@ -0,0 +1,17 @@
+export class WeatherService {
+  getWeatherByCity(city) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a48b5f436f847ffc1f823226c4f99594`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
